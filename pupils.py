cb84036facb06974ce6028182613f674cd59f0e1
AlyonaGM <Alenushka620@gmail.com>
2016-10-21 00:11:35 +0300

    5 task

---
+++ pupils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+print("Входные данные ")
+
+x = int(input())
+y = int(input())
+z = int(input())
+print("Выходные данные")
+class1 = (x // 2) + (x % 2)
+class2 = (y // 2) + (y % 2)
+class3 = (z // 2) + (z % 2)
+print (class1 + class2 + class3)
