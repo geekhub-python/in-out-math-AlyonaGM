85768b098004f56849a3015928a97935e58513db
AlyonaGM <Alenushka620@gmail.com>
2016-10-21 23:43:54 +0300

    6 task

---
+++ laces.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+print("Входные данные")
+
+a = int(input())
+b = int(input())
+l = int(input())
+N = int(input())
+
+print("Выходные данные")
+print(((N - 1) * 2 * b) + ((N - 1) * 2 * a + a) + 2 * l)
